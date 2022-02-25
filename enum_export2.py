73c986ca71ae50cc2f5a8904da1fc8ead7e112b4
0xdecafdad <shaunderstine@gmail.com>
2022-02-25 05:54:15 +0000

    Implemented Rule 1

--- enum_export2.py
+++ enum_export2.py
@@ -16,13 +16,38 @@ malware_path = sys.argv[1] # path to directory with malware
 # add try/catch block in case of invalid path
 malware_dir = os.scandir(malware_path) # returns list of os.DirEntry objects
 
+# global variables? (maybe bad?)
+# if key exists in this dict, add one to its value
+# otherwise, add it to dict
+rule1 = {} 
+
 # accesses each file in directory
 for malware_file in malware_dir:
     pe = pefile.PE(malware_file)
 
     if hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
+        # prints out address for each function in current file
         for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-            print("%s" % (hex(exp.address + pe.OPTIONAL_HEADER.ImageBase)))
+            # for finding address of function: (hex(exp.address + pe.optional_header.imagebase))
+            function_address = (hex(exp.address + pe.OPTIONAL_HEADER.ImageBase))
+            if function_address in rule1:
+                rule1[function_address] += 1
+            else:
+                rule1[function_address] = 1
             # end of inner for loop
-    print("\n")
+
+    # determines if current file is malware based on parameters stated earlier
+    is_malware = False
+
+    # check for rule 1
+    for value in rule1.values():
+        if value >= 3:
+            is_malware = True
+
+    # print message based on whether or not malware was detected
+    if is_malware == True:
+        print("%s: Malware detected!" % malware_file.name)
+    else:
+        print("%s: No malware detected." % malware_file.name)
+    rule1.clear() # clears dictionary for next file
     # end of outer for loop
